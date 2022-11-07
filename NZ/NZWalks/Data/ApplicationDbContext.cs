8d4c1c3bdb251cc37c7680d6aef089e5bf6386c2
Pranab4712 <pranabofficial4712@gmail.com>
2022-11-07 21:07:20 +0530

    End of Section 2

---
+++ NZ/NZWalks/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.Models.Domain;
+
+namespace NZWalks.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> Options):base(Options)
+        {
+
+        }
+        public DbSet<Region> Regions { set; get; }
+        public DbSet<Walk> Walks { set; get; }
+        public DbSet<WalkDifficulty> WalkDifficulties { set; get; }
+    }
+}
---
+++ NZ/NZWalks/Models/Domain/Walk.cs
@@ -0,0 +1,13 @@
+﻿namespace NZWalks.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Length { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+    }
+}
